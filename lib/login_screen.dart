30d2efef608885e2fb3c99973cacc3d784afcf80
Vanessa-Maria2 <vanessa.silva5205@gmail.com>
2026-05-18 21:50:36 -0300

    feat: adiciona ícone central e melhora estilo dos botões e bordas

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -12,71 +12,103 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Minha aplicação',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 5),
-              Text(
-                'Bem vindo de volta!',
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-              ),
-              SizedBox(height: 24),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Usuário',
-                  border: OutlineInputBorder(),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Color(0xFFEEF2FF),
+                  child: Icon(
+                    Icons.lock_outline,
+                    size: 30,
+                    color: Color(0xFF4F46E5),
+                  ),
                 ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
+                SizedBox(height: 10),
+                Text(
+                  'Auth',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-              ),
-              SizedBox(height: 8),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text('Esqueceu a senha?'),
+                SizedBox(height: 5),
+                Text(
+                  'Seja Bem vindo!',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
                 ),
-              ),
-              SizedBox(height: 8),
+                SizedBox(height: 24),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Usuário',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: Color(0xFFCBD5E1),
+                        width: 1.5,
+                      ),
+                    ),
+                    prefixIcon: Icon(Icons.person_outline,  color: Color(0xFF6B7280)),
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: Color(0xFFCBD5E1),
+                        width: 1.5,
+                      ),
+                    ),
+                    prefixIcon: Icon(Icons.lock_outline, color: Color(0xFF6B7280)),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Esqueceu a senha?'),
+                  ),
+                ),
+                SizedBox(height: 8),
 
-              SizedBox(
-                width: double.infinity,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF4F46E5),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
+                SizedBox(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF4F46E5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
                     ),
                   ),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
                 ),
-              ),
-              const SizedBox(height: 18),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Não tem uma conta?'),
-                  TextButton(onPressed: () {}, child: Text('Cadastre-se')),
-                ],
-              ),
-            ],
+                const SizedBox(height: 18),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Não tem uma conta?'),
+                    TextButton(onPressed: () {}, child: Text('Cadastre-se')),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
